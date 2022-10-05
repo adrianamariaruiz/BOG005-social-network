828ae38f199106170d0683c53de5f6f06923d8ba
Adriana Ruiz <adriannaruiz24@gmail.com>
2022-10-05 13:34:18 -0500

    inicializando firestore

---
+++ src/lib/firebase/configFirestore.js
@@ -0,0 +1,9 @@
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
+import { app } from '../firebase/authFirebase.js';
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export const savePost = (description) => {
+    addDoc(collection(db, "posts"), { description });
+}
