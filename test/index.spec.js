ff7d2da7663fd2520a534e571270a20c93dda14d
jeniferSamper <jenifersamperusb@gmail.com>
2022-10-18 11:27:14 -0500

    test de ruteado completado

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,33 @@
 // importamos la funcion que vamos a testear
 import { onNavigate } from '../src/main.js'
-import { mockTemplateSignIn, mockRoutes } from './mocks.js'
+import { mockRoutes } from './mocks.js'
+
 
 jest.mock('../src/lib/firebase/authFirebase.js');
 jest.mock('../src/lib/firebase/configFirestore.js');
 
+
 describe('test onNavigate', () => {
   it('change of view singIn', () => {
-    document.body.innerHTML = '<main id="root"></main>'
-    onNavigate('/signUp', mockRoutes);
-    console.log(document.getElementById('root').textContent)
-    expect(document.getElementById('root').textContent.trim()).toEqual('vista de SignIn');
+    document.body.innerHTML = `<main id="root"></main>`;
+    onNavigate('/', mockRoutes);
+    expect(document.getElementById('root').textContent).toEqual('vista de SignIn');
   });
 });
+
+describe('test onNavigate', () => {
+  it('change of view singUp', () => {
+    document.body.innerHTML = `<main id="root"></main>`;
+    onNavigate('/signUp', mockRoutes);
+    expect(document.getElementById('root').textContent).toEqual('vista de SignUp');
+  });
+});
+
+describe('test onNavigate', () => {
+  it('change of view principalPage', () => {
+    document.body.innerHTML = `<main id="root"></main>`;
+    onNavigate('/principalPage', mockRoutes);
+    expect(document.getElementById('root').textContent).toEqual('Publicar mock');
+  });
+});
+
